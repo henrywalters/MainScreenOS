4f420babfb719520c3ac6eb0cb51799cd1d923f8
mikulak25 <adrianmikulak@gmail.com>
2017-05-20 21:23:12 -0400

    Built form style

--- form.php
+++ form.php
@@ -5,6 +5,9 @@
   <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 <html>
 <div id = "form"> </div>
+<style>
+
+</style>
 </html>
 
 <script>
@@ -21,8 +24,8 @@ function Form(id,x,y,title,h,w,color){
 }
 
 Form.prototype.draw = function (){
-	var html = "<div class='container' id='" + this.id + "' style='border:1px solid black;position:absolute;background-color:" + this.color + ";height:" + this.h + "px;width:" + this.w + "px;top:" + this.y + "px;left:" + this.x + "px'>";
-	html += "<header style='border: 1px solid black;position:relative;top:20px;background-color: gray;'> </header>"
+	var html = "<div class='container' id='" + this.id + "' style='border:2px solid #34495e;position:absolute;background-color:" + this.color + ";height:" + this.h + "px;width:" + this.w + "px;top:" + this.y + "px;left:" + this.x + "px'>";
+	html += "<header id='formHeader'style='border-bottom: 2px solid #34495e;background-color: #95a5a6;position:relative;bottom:20px;'><h2 style='height:10px;'></h2><div id='closeForm' style='float:right;position:relative;bottom:25px;right:5px;font-family:arial;'>x</div></header>"
 	html += "</div>";
 	$('#form').append(html);
 }
